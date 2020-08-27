09787d50975bee7f45b47399ad5ac45d84d7ae8f
Brendon Mota <bw.bwctm@gmail.com>
2020-08-27 17:59:30 -0300

    Adding a tutorial lib as an example

--- app.py
+++ app.py
@@ -1,29 +1,23 @@
 import win
 import market
 import arrayutils
-from functools import reduce
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
+import tutorial
 
 start_date = win.get_start_date_period()
 initials = win.get_complete_initials()
-look_back = 1
 
 rates = market.get_rates_from_start_date(initials, start_date)
-closes = list(map(lambda val: val['close'], rates))
-dataset = arrayutils.get_diffs(closes)
+dataset = list(map(lambda val: val['close'], rates))
+# dataset = arrayutils.get_vector_diffs(dataset)
 
-train_size = int(len(dataset) * 0.67)
-test_size = len(dataset) - train_size
+# train_size = int(len(dataset) * 0.67)
+# test_size = len(dataset) - train_size
+# train, test = dataset[:train_size], dataset[train_size:]
 
-train, test = dataset[:train_size], dataset[train_size:]
+# trainX, trainY = train[1:], train[:len(train)-1]
+# testX, testY = test[1:], test[:len(train)-1]
 
-trainX, trainY = train[:len(train)], train[1:]
-testX, testY = test[:len(train)], test[1:]
 
-model = Sequential()
-model.add(LSTM(4, input_shape=(1, look_back)))
-model.add(Dense(1))
-model.compile(loss='mean_squared_error', optimizer='adam')
-model.fit(trainX, trainY, epochs=100, batch_size=1, verbose=2)
+dataset = list(map(lambda val: [val], dataset))
+print(len(dataset))
+tutorial.main(dataset, 1, 300)
--- arrayutils.py
+++ arrayutils.py
@@ -1,4 +1,4 @@
-def get_diffs(array):
+def get_vector_diffs(array):
     diffs = []
     for i in range(1, len(array)):
         diffs.append(array[i] - array[i-1])
