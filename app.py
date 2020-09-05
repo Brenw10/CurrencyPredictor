0ee8f37fd54e1d805eb496ce693bc6736d0aaf0f
Brendon Mota <bw.bwctm@gmail.com>
2020-09-05 00:06:30 -0300

    Creating methods that abstract the prediction

--- app.py
+++ app.py
@@ -1,38 +1,54 @@
-import win
-import market
-import arrayutils
-import tutorial
+# import win
+# import market
+# import arrayutils
+# import tutorial
 import predictor
-import numpy
-from datetime import datetime
+# import numpy
+# from datetime import datetime
 import matplotlib.pyplot as plt
 
-start_date = win.get_start_date_period()
-initials = win.get_complete_initials()
+# start_date = win.get_start_date_period()
+# initials = win.get_complete_initials()
 
-rates = market.get_rates_from_start_date(initials, start_date)
-# rates = market.get_rates_from_start_date(initials, datetime(2020, 8, 31, 11))
-dataset = list(map(lambda val: [val['close']], rates))
-train = predictor.normalize(dataset)
+# rates = market.get_rates_from_start_date(initials, start_date)
+# # rates = market.get_rates_from_start_date(initials, datetime(2020, 8, 31, 11))
+# dataset = list(map(lambda val: [val['close']], rates))
+# train = predictor.normalize(dataset)
+
+# look_back = 1
+# trainX, trainY = tutorial.create_dataset(train, look_back)
+# trainX = numpy.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
+
+# predictor.train(trainX, trainY, 100, look_back)
+# result = predictor.get(trainX)
+
+# future = result.reshape(1, -1)[0]
+# for i in range(100):
+#     p = numpy.array(list(map(lambda val: [val], future)))
+#     p = numpy.reshape(p, (p.shape[0], 1, p.shape[1]))
+#     predict = predictor.get(p)
+#     last = predict[len(predict) - 1]
+#     future = numpy.append(future, last)
+
+# future = numpy.array(list(map(lambda val: [val], future)))
+
+# plt.plot(dataset)
+# plt.plot(predictor.unnormalize(result))
+# plt.plot(predictor.unnormalize(future))
+# plt.show()
 
 look_back = 1
-trainX, trainY = tutorial.create_dataset(train, look_back)
-trainX = numpy.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
+epochs = 100000
+sequence = [10, 20, 30, 40, 50, 30, 40, 50, 60, 70, 50, 60, 70, 80, 90]
+next_sequence = [70, 80]
 
-predictor.train(trainX, trainY, 100, look_back)
-result = predictor.get(trainX)
+predictor.train_sequence(sequence, epochs, look_back)
+predict = predictor.get(sequence + next_sequence, look_back)
 
-future = result.reshape(1, -1)[0]
-for i in range(100):
-    p = numpy.array(list(map(lambda val: [val], future)))
-    p = numpy.reshape(p, (p.shape[0], 1, p.shape[1]))
-    predict = predictor.get(p)
-    last = predict[len(predict) - 1]
-    future = numpy.append(future, last)
+unpredictable = predictor.get_scaler_format(sequence[0:look_back])
+predict = unpredictable + predict.tolist()
+print(predict)
 
-future = numpy.array(list(map(lambda val: [val], future)))
-
-plt.plot(dataset)
-plt.plot(predictor.unnormalize(result))
-plt.plot(predictor.unnormalize(future))
+plt.plot(sequence)
+plt.plot(predict)
 plt.show()
--- predictor.py
+++ predictor.py
@@ -2,26 +2,62 @@ from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
+import numpy
 
 scaler = MinMaxScaler(feature_range=(0, 1))
 model = Sequential()
 
 
 def normalize(dataset):
-    return scaler.fit_transform(dataset)
+    dataset = get_scaler_format(dataset)
+    dataset = scaler.fit_transform(dataset)
+    return get_scaler_unformat(dataset)
 
 
 def unnormalize(result):
     return scaler.inverse_transform(result)
 
 
-def train(trainX, trainY, epochs, look_back=1):
+def get_scaler_format(dataset):
+    return list(map(lambda x: [x], dataset))
+
+
+def get_scaler_unformat(dataset):
+    return list(map(lambda x: x[0], dataset))
+
+
+def train(trainX, trainY, epochs, look_back):
     model.add(LSTM(4, input_shape=(1, look_back)))
     model.add(Dense(1))
     model.compile(loss='mean_squared_error', optimizer='adam')
     model.fit(trainX, trainY, epochs=epochs, batch_size=1, verbose=2)
 
 
-def get(dataset, unnorm=False):
+def get(sequence, look_back):
+    dataset = normalize(sequence)
+    dataset = create_input(dataset, look_back, False)
+    dataset = numpy.reshape(dataset, (dataset.shape[0], 1, dataset.shape[1]))
     predict = model.predict(dataset)
-    return unnormalize(predict) if unnorm else predict
+    return unnormalize(predict)
+
+
+def train_sequence(sequence, epochs, look_back):
+    sequence = normalize(sequence)
+    trainX = create_input(sequence, look_back, True)
+    trainY = create_output(sequence, look_back)
+    trainX = numpy.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
+    train(trainX, trainY, epochs, look_back)
+
+
+def create_input(sequence, look_back, train=False):
+    data = []
+    for i in range(len(sequence)-look_back+1):
+        value = sequence[i:i+look_back]
+        data.append(value)
+    data = numpy.array(data)
+    return data[:-1] if train else data
+
+
+def create_output(sequence, look_back):
+    data = sequence[look_back:]
+    return numpy.array(data)
