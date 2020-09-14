48d9f7bbb9a4d52ac3e053013deafc141932f7ef
Brendon Mota <bw.bwctm@gmail.com>
2020-09-14 12:09:55 -0300

    Re-predict once graphic closes

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import time
 import win
 import market
 import predictor
@@ -6,27 +5,40 @@ import config
 from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
 
-start_time = time.time()
-
+date = datetime.now()
 look_back = config.get()["predictor"]['look_back']
 look_beyond = config.get()["predictor"]['look_beyond']
 epochs = config.get()["predictor"]['epochs']
 train_size = 1 - config.get()["predictor"]['train_size']
 
-date = datetime.now()
-start_date = win.get_start_date_period()
-end_date = datetime(date.year, date.month, date.day) + timedelta(days=1)
-initials = win.get_complete_initials()
 
-rates = market.get_rates_from_date(initials, start_date, end_date)
-sequence = list(map(lambda val: val['close'], rates))
+def get_sequence():
+    start_date = win.get_start_date_period()
+    end_date = datetime(date.year, date.month, date.day) + timedelta(days=1)
+    initials = win.get_complete_initials()
 
-predictor.train_sequence(sequence[int(len(sequence) * train_size):], epochs, look_back)
-predict = predictor.forecast(sequence, look_back, look_beyond)
+    rates = market.get_rates_from_date(initials, start_date, end_date)
+    return list(map(lambda val: val['close'], rates))
 
-print("--- %s seconds ---" % (time.time() - start_time))
-plt.plot(sequence + predict[len(sequence):], label='Forecast')
-plt.plot(predict[:len(sequence)], label='Predict')
-plt.plot(sequence, label='Real')
-plt.legend()
-plt.show()
+
+def train():
+    sequence = get_sequence()
+    size = int(len(sequence) * train_size)
+    predictor.train_sequence(sequence[size:], epochs, look_back)
+
+
+def predict():
+    sequence = get_sequence()
+    result = predictor.forecast(sequence, look_back, look_beyond)
+
+    plt.plot(sequence + result[len(sequence):], label='Forecast')
+    plt.plot(result[:len(sequence)], label='Predict')
+    plt.plot(sequence, label='Real')
+    plt.legend()
+    plt.show()
+
+    predict()
+
+
+train()
+predict()
